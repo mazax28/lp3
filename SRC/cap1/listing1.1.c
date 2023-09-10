cfd4bebcd2be11c5e256913ab1054df453e9fb4b
mazax28 <marcoskrause2002@fpuna.edu.py>
2023-09-09 20:56:57 -0400

    marcos_zarate-1

---
+++ SRC/cap1/listing1.1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Imprime "Hola, mundo" en la pantalla
+    printf("Hola, mundo\n");
+
+    // Retorna 0 para indicar que el programa se ejecutó con éxito
+    return 0;
+}
