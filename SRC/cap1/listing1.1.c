b108770620683d0679f73824f9597d8fdd847c00
mazax28 <marcoskrause2002@fpuna.edu.py>
2023-09-17 11:19:00 -0400

    marcos_zarate-2

--- SRC/cap1/listing1.1.c
+++ SRC/cap1/listing1.1.c
@@ -1,9 +1,25 @@
+
+
+/*INCLUYE LA BIBLIOTECA ESTANDAR DE ENTRADA Y SALIDA*/
 #include <stdio.h>
 
-int main() {
-    // Imprime "Hola, mundo" en la pantalla
-    printf("Hola, mundo\n");
+/*INCLUYE ARCHIVO ENCABEZADO QUE CONTIENE LA DECLARACION DE LA FUNCION reciprocal*/
+#include "listing1.3.hpp"
 
-    // Retorna 0 para indicar que el programa se ejecutó con éxito
+/*ESTA LIBRERIA SE TUVO QUE AGREGAR PARA UTILIZAR LA FUNCION atoi*/
+#include<stdlib.h>
+
+int main (int argc, char **argv){
+
+    int i;
+
+    /*CONVIERTE EL PRIMER ARGUMENTO DE LA LINEA DE COMANDOS EN UN ENTERO*/
+    i = atoi(argv[1]);
+
+
+    /*IMPRIME EL RESULTADO DEL CALCULO DEL RECIPROCO DEL NUMERO*/
+    printf("the reciprocal of %d is %g\n ",i, reciprocal(i));
+
+
     return 0;
 }
